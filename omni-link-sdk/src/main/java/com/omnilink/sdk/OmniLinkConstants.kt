24955be95595375d15fcf48c4d99087b999dc36a
Obieda <bis.2410423@eia.edu.eg>
2026-09-18 05:37:20 +0300

    feat: add agent gateway protocol constants

--- omni-link-sdk/src/main/java/com/omnilink/sdk/OmniLinkConstants.kt
+++ omni-link-sdk/src/main/java/com/omnilink/sdk/OmniLinkConstants.kt
@@ -1,6 +1,12 @@
 package com.omnilink.sdk
 
 object OmniLinkConstants {
+    const val SDK_VERSION = "1.1.0"
+    const val CURRENT_PROTOCOL_VERSION = 3
+
     const val PERMISSION_BIND_EXTENSION = "com.omnilink.sdk.permission.BIND_EXTENSION"
     const val ACTION_EXTENSION_BIND = "com.omnilink.sdk.action.EXTENSION_BIND"
+
+    const val PERMISSION_BIND_AGENT = "com.omnilink.sdk.permission.BIND_AGENT"
+    const val ACTION_AGENT_GATEWAY_BIND = "com.omnilink.sdk.action.AGENT_GATEWAY_BIND"
 }
