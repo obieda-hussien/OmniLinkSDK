0d9268ee53a72385dc3c17e78da26fd28f2ddf45
Obieda <bis.2410423@eia.edu.eg>
2026-09-19 13:38:54 +0300

    deps: align JVM transport runtime with Kotlin 2.4.20

--- omni-link-transport/build.gradle.kts
+++ omni-link-transport/build.gradle.kts
@@ -14,11 +14,11 @@ kotlin {
 }
 
 dependencies {
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.7.0")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.8.1")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.11.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.11.0")
 
     testImplementation("junit:junit:4.13.2")
-    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.8.1")
+    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.11.0")
 }
 
 java {
