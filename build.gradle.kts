f9d820afe79e7c22f12212646bbbb53fb5161690
Obieda <bis.2410423@eia.edu.eg>
2026-09-19 13:45:31 +0300

    build: pin Kotlin 2.4.10 for current CodeQL support

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     id("com.android.library") version "8.13.2" apply false
-    id("org.jetbrains.kotlin.android") version "2.4.20" apply false
-    id("org.jetbrains.kotlin.jvm") version "2.4.20" apply false
-    id("org.jetbrains.kotlin.plugin.serialization") version "2.4.20" apply false
+    id("org.jetbrains.kotlin.android") version "2.4.10" apply false
+    id("org.jetbrains.kotlin.jvm") version "2.4.10" apply false
+    id("org.jetbrains.kotlin.plugin.serialization") version "2.4.10" apply false
 }
