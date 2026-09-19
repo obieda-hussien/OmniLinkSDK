589bb58f657f7fb5c00dc37667a3b31414447456
Obieda <bis.2410423@eia.edu.eg>
2026-09-19 13:38:46 +0300

    build: align AGP and Kotlin toolchain for 1.4.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-    id("com.android.library") version "8.5.1" apply false
-    id("org.jetbrains.kotlin.android") version "2.0.0" apply false
-    id("org.jetbrains.kotlin.jvm") version "2.0.0" apply false
-    id("org.jetbrains.kotlin.plugin.serialization") version "2.0.0" apply false
+    id("com.android.library") version "8.13.2" apply false
+    id("org.jetbrains.kotlin.android") version "2.4.20" apply false
+    id("org.jetbrains.kotlin.jvm") version "2.4.20" apply false
+    id("org.jetbrains.kotlin.plugin.serialization") version "2.4.20" apply false
 }
